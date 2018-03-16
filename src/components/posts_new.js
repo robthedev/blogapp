8bd55f1b3e0303d8ea3a9c3fb239bcebf88f9119
robthewebdev <robthewebdev@gmail.com>
2018-03-16 15:27:30 -0500

    reduxform

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
     render() {
         return (
-            <div>
-                Postnew!
-            </div>
+            <form action="">
+                <Field
+                    name="title"
+                    componet={}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+export default  reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducers_posts';
 
 const rootReducer = combineReducers({
-  state: PostsReducer
+    posts: PostsReducer,
+    form: formReducer
 });
 
 export default rootReducer;
