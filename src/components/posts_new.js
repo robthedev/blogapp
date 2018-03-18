a020d52388183f730fe1a21c50d87460dff7e600
robthewebdev <robthewebdev@gmail.com>
2018-03-18 13:02:48 -0500

    got file watch to work, added fields to redux form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,18 +2,35 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        )
+    }
     render() {
         return (
-            <form action="">
+            <form>
                 <Field
+                    label="Title"
                     name="title"
-                    componet={}
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
                 />
             </form>
         );
     }
 }
-
 export default  reduxForm({
     form: 'PostsNewForm'
 })(PostsNew);
