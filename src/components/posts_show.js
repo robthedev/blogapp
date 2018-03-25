30f13e479ca98d5298085230f8ac243c1fef7b98
robthewebdev <robthewebdev@gmail.com>
2018-03-24 21:47:25 -0500

    delete post functionality working

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchPost} from "../actions";
+import { deletePost} from "../actions";
 
 class PostsShow extends Component {
     componentDidMount() {
-       const id = this.props.match.params.id;
+       const id  = this.props.match.params.id;
         this.props.fetchPost(id);
+    }
+
+    onDeleteClick() {
+        const id  = this.props.match.params.id;
+
+        this.props.deletePost(id, () => {
+            this.props.history.push('/');
+        });
     }
     render() {
         const { post } = this.props;
@@ -18,6 +27,12 @@ class PostsShow extends Component {
     return (
             <div>
                 <Link to="/">Back to home</Link>
+                <button
+                    className="pull-xs-right"
+                    onClick={this.onDeleteClick.bind(this)}
+                >
+                    delete post
+                </button>
                 <h3>{post.title}</h3>
                 <h4>Cate: {post.categories}</h4>
                 <p>{post.content}</p>
@@ -30,4 +45,4 @@ function mapStateToProps( { posts }, ownProps) {
     return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
